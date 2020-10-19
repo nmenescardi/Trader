b1a454bbc1bba2fad98c6bea60efe46ee8465bdd
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-10-19 10:51:40 -0300

    fix data issue

--- StrategiesManager/yFinanceDataProvider.py
+++ StrategiesManager/yFinanceDataProvider.py
@@ -2,5 +2,5 @@ import yfinance as yf
 
 class yFinanceDataProvider:
 
-	def get(self, symbol, period="1d", interval="5m"):
+	def get(self, symbol, period="2d", interval="5m"):
 		return yf.Ticker(symbol).history(period=period, interval=interval, debug=False)
