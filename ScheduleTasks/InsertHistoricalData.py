339c27023545f726b119ab40f6bcbb765b4fdcb5
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-11-26 11:42:17 -0300

    add schedule task to insert historical data

---
+++ ScheduleTasks/InsertHistoricalData.py
@@ -0,0 +1,35 @@
+from Data.HistoricalData import HistoricalData
+from DataFeed.AlphaVantage import AlphaVantage
+
+class InsertHistoricalData:
+
+	def __init__(self, full_data = False):
+		if full_data:
+			# Two years of data
+			self.amount_years = 2
+			self.amount_months = 12
+		else:
+			# Only last two month
+			self.amount_years = 1
+			self.amount_months = 2
+
+
+	def run(self):
+		for ticker in ['AAPL']: #TODO: get from list
+
+			df = AlphaVantage().get_data(ticker, amount_years = self.amount_years, amount_months = self.amount_months)
+
+			historical_data = HistoricalData()
+
+			for index, row in df.iterrows():
+				historical_data.insert_stock_price(
+					ticker = ticker,
+					time_price = str(index),
+					open_price = str(row['open']),
+					high_price = str(row['high']),
+					low_price = str(row['low']),
+					close_price = str(row['close']),
+					volume = str(row['volume']),
+					timeframe = '5m',
+				)
+				#print(index, row['open'], row['high'], row['low'], row['close'], row['volume'])
